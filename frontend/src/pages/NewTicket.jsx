16064cf9b5b5185c2dd30c628afd34a6ef3896ef
Moises Valdez <moisesvaldez075@gmail.com>
2022-03-23 11:09:55 -0700

    added a new ticket page

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function NewTicket() {
+    const { user } = useSelector((state) => state.auth);
+    const [name] = useState(user.name);
+    const [email] = useState(user.email);
+    const [product, setProduct] = useState('iPhone');
+    const [description, setDescription] = useState('');
+
     return (
-        <div>
-            <h1>New Ticket</h1>
-        </div>
+        <>
+            <section className='heading'>
+                <h1>Create New Ticket</h1>
+                <p>Please fill out the form below</p>
+            </section>
+
+            <section className='form'>
+                <div className='form-group'>
+                    <label htmlFor='name'>Customer Name</label>
+                    <input
+                        type='text'
+                        value={name}
+                        className='form-control'
+                        disabled
+                    />
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='email'>Customer Email</label>
+                    <input
+                        type='text'
+                        value={name}
+                        className='form-control'
+                        disabled
+                    />
+                </div>
+            </section>
+        </>
     );
 }
 
